1bec9fccfa79a415bfecb9f688f1dc7fceccf2fd
Johannes Asbahr <hannes-asbahr@hotmail.de>
2018-10-23 12:07:15 +0200

    no twitter login anymore

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,14 +1,13 @@
 import React from "react"
 import { connect } from "react-redux"
-import { startLoginGoogle, startLoginTwitter } from "../actions/auth"
+import { startLoginGoogle } from "../actions/auth"
 
-export const LoginPage = ({ startLoginGoogle, startLoginTwitter }) => (
+export const LoginPage = ({ startLoginGoogle }) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control</p>
             <button className="button" onClick={startLoginGoogle}>Login with Google</button>
-            <button className="button button--twitter" onClick={startLoginTwitter}>Login with Twitter</button>  
         </div>
     </div>
 )
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -22,9 +22,8 @@ firebase.initializeApp(config);
 
 const database = firebase.database()
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
-const twitterAuthProvider = new firebase.auth.TwitterAuthProvider()
 
-export { firebase, googleAuthProvider, twitterAuthProvider, database as default }
+export { firebase, googleAuthProvider, database as default }
 
 // // child_removed
 // database.ref("expenses").on("child_removed", (snapshot) => {
